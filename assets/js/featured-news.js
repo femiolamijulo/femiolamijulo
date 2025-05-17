8865d95b919971e973f4051a20bc4f93c592c1f0
Femi Olamijulo <olufemi_olamijulo@gsd.harvard.edu>
2025-05-17 12:22:11 -0400

    deleted:    assets/_83A2466.jpg     deleted:    jpg/pngs and compressed all images to webp format

--- assets/js/featured-news.js
+++ assets/js/featured-news.js
@@ -1,7 +1,7 @@
 const allNewsItems = [
   {
     title: "Award for Outstanding Leadership",
-    img: "../assets/news_photos/Outstanding.jpg",
+    img: "../assets/news_photos/Outstanding.webp",
     alt: "Outstanding Leadership Award in Urban Planning",
     url: "https://www.gsd.harvard.edu/urban-planning-design/fellowships-prizes-and-travel-programs/award-for-outstanding-leadership-in-urban-planning-and-urban-design/",
     description: "Recipient of the 2024 Award for Outstanding Leadership in Urban Planning and Urban Design.",
@@ -10,7 +10,7 @@ const allNewsItems = [
   },
   {
     title: "Rappaport Connects: Alumni Spotlight",
-    img: "../assets/news_photos/Rappaport.png",
+    img: "../assets/news_photos/Rappaport.webp.png",
     alt: "Rappaport Interview",
     url: "https://www.youtube.com/watch?v=SP8k8oyuAMc",
     description: "Interviewed on planning, equity, and GIS during and after Harvard.",
@@ -19,7 +19,7 @@ const allNewsItems = [
   },
   {
     title: "Harvard CGA March 2024 Update",
-    img: "../assets/news_photos/Everett_HKS.jpg",
+    img: "../assets/news_photos/Everett_HKS.webp.png",
     alt: "CGA 2024 News Archive",
     url: "https://gis.harvard.edu/news/archive/2024-03",
     description: "Work on spatial storytelling featured in the Center for Geographic Analysis’ spring roundup.",
@@ -37,7 +37,7 @@ const allNewsItems = [
   },
   {
     title: "Presentation at Everett City Hall",
-    img: "../assets/news_photos/everett_independent.jpg",
+    img: "../assets/news_photos/everett_independent.webp",
     alt: "City Hall Presentation",
     url: "https://everettindependent.com/2023/05/10/harvard-kennedy-students-present-at-city-hall/",
     description: "Shared green corridor concepts with local officials and residents.",
@@ -46,7 +46,7 @@ const allNewsItems = [
   },
   {
     title: "Everett Community Presentation",
-    img: "../assets/news_photos/everett_advocate.png",
+    img: "../assets/news_photos/everett_advocate.webp",
     alt: "Everett Community Presentation",
     url: "https://advocatenews.net/everett/news/harvard-kennedy-school-students-present-at-city-hall/",
     description: "Featured in the Advocate for presenting mixed-use corridor concepts to local leaders in Everett, MA.",
@@ -55,7 +55,7 @@ const allNewsItems = [
   },
   {
     title: "2024 Esri Innovation Award",
-    img: "../assets/news_photos/EIP_award.png",
+    img: "../assets/news_photos/EIP_award.webp",
     alt: "Esri Innovation Award",
     url: "https://gis.harvard.edu/news/2024-eip-award-winner",
     description: "Recognized for using geospatial storytelling in equity-focused planning.",
@@ -64,7 +64,7 @@ const allNewsItems = [
   },
   {
     title: "GreenEconomiX: SDG Project Spotlight",
-    img: "../assets/news_photos/GLC.png",
+    img: "../assets/news_photos/GLC.webp.png",
     alt: "GreenEconomiX SDG Project",
     url: "https://www.leadership-challenge.org/sdg-projects/greeneconomix%3A-minimizing-barriers%2C-maximizing-impact",
     description: "Recognized by the Leadership Challenge for work on minimizing barriers to green infrastructure in Africa.",
@@ -73,7 +73,7 @@ const allNewsItems = [
   },
   {
     title: "Harvard HCIP Fellowship",
-    img: "../assets/news_photos/Headshot.png",
+    img: "../assets/news_photos/Headshot.webp",
     alt: "HCIP Fellowship Announcement",
     url: "https://www.hks.harvard.edu/centers/mrcbg/programs/hcip/fellows",
     description: "Selected as a 2024 Fellow of the Harvard Climate and Infrastructure Policy Program.",
@@ -82,7 +82,7 @@ const allNewsItems = [
   },
   {
     title: "Stories We Should Tell",
-    img: "../assets/news_photos/Stories_we_should_tell.png",
+    img: "../assets/news_photos/Stories_we_should_tell.webp",
     alt: "Storytelling Exhibit",
     url: "https://www.gsd.harvard.edu/exhibition/stories-we-should-tell/",
     description: "Featured in a GSD exhibit uplifting African and diasporic spatial storytelling.",
